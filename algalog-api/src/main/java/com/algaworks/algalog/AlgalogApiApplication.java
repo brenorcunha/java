f1e70e5cd77de1a79bd543a3a1c2c1cb474d2f19
Breno R.Cunha <breno.rcunha@icloud.com>
2022-07-20 20:06:52 -0300

    200722changes

--- algalog-api/src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
+++ algalog-api/src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
@@ -11,3 +11,24 @@ public class AlgalogApiApplication {
 	}
 
 }
+/*
+ * FAZER REQUISICOES:
+ * Executar: No terminal do VS dá "java -jar <nomedojarsnapshot>.jar"
+ * GET= Só enviar GET pra http://localhost:8080/clientes
+ * POST= No campo BODY coloca o JSON com dados a serem add. Ex:
+ * {
+        "id": 0,
+        "nome": "Humberto A. da Cunha",
+        "email": "bettocunha@alga.com",
+        "telefone": "(12) 92000-0000"
+    }
+	PUT: Aí precisa mandar o dado a ser alterado na uRL. Ex:
+	http://localhost:8080/clientes/3 (Quero alterar dados do cliente de ID 3).
+
+	 No BODY:
+	 {
+        "nome": "Humberto A. da Cunha",
+        "email": "humberto_da_cunha@alga.com",
+        "telefone": "(12) 92000-0000"
+    }
+ */
